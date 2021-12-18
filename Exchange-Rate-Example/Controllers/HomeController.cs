559bda2340ca6cbfb8a18686dcc6ea2c2c4a9fdb
MESUT KAYA <mesutkaya2000@gmail.com>
2021-12-18 18:05:56 +0300

    First commit

---
+++ Exchange-Rate-Example/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Exchange_Rate_Example.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exchange_Rate_Example.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<ExchangeRateModel> rateTypeList = new List<ExchangeRateModel>();
+
+        public ActionResult Index()
+        {
+            using (WebClient wc = new WebClient())
+            {
+                var path = ConfigurationManager.AppSettings["urlApi"];
+                var json = wc.DownloadString(path);
+                dynamic parsed = JsonConvert.DeserializeObject(json);
+                JsonToList(parsed);
+            }
+            return View(rateTypeList);
+        }
+        private void JsonToList(dynamic obj)
+        {
+            foreach (var result in obj)
+            {
+                foreach (var item in result)
+                {
+                    ExchangeRateModel rateType = new ExchangeRateModel();
+                    rateType.RateTypeName = result.Name;
+                    rateType.Names = item.isim;
+                    rateType.Buying = item.alis;
+                    rateType.Selling = item.satis;
+                    rateType.Changing = (Convert.ToDecimal(item.degisim) / 100);
+                    rateTypeList.Add(rateType);
+                }
+            }
+        }
+    }
+}
---
+++ Exchange-Rate-Example/Models/ExchangeRateModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Exchange_Rate_Example.Models
+{
+    public class ExchangeRateModel
+    {
+        public string RateTypeName { get; set; }
+        public string Names { get; set; }
+        public decimal Buying { get; set; }
+        public decimal Selling { get; set; }
+        public decimal Changing { get; set; }
+    }
+}
